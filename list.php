f6f380a7abb279b8870154a6c26114b3a1c54190
wjames5 <will@tekimaki.com>
2009-10-01 18:53:05 +0000

    only display active games

--- list.php
+++ list.php
@@ -1,8 +1,8 @@
 <?php
-// $Header: /cvsroot/bitweaver/_bit_games/list.php,v 1.1 2009/09/15 14:54:44 wjames5 Exp $
+// $Header: /cvsroot/bitweaver/_bit_games/list.php,v 1.2 2009/10/01 18:53:05 wjames5 Exp $
 // Copyright (c) 2004 bitweaver Games
 // All Rights Reserved. See copyright.txt for details and a complete list of authors.
-// Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details.
+// Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See http://www.gnu.org/copyleft/lesser.html for details.
 // Initialization
 require_once( '../bit_setup_inc.php' );
 
@@ -15,8 +15,10 @@ require_once( GAMES_PKG_PATH.'lookup_game_inc.php' );
 // Now check permissions to access this page
 $gBitSystem->verifyPermission( 'p_games_play' );
 
-// Create new games object
-$gamesList = $gGameSystem->getList( $_REQUEST );
+// Get list of active games
+$listHash = $_REQUEST;
+$listHash['active'] = TRUE;
+$gamesList = $gGameSystem->getList( $listHash );
 $gBitSmarty->assign_by_ref( 'gamesList', $gamesList );
 
 // getList() has now placed all the pagination information in $_REQUEST['listInfo']
